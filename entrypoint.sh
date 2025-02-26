3be456b0821e1eaebde52abf9e8b3b4cb2809068
patleo <plsweeney25@gmail.com>
2025-02-26 15:39:12 -0500

    test: remove dapr from dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,9 @@ echo "entrypoint.sh executed..."
 # Exit immediately if any command exits with a non-zero status
 set -e
 
-dapr run --app-id interviewstopwatch --app-port 5173 --dapr-http-port 3500 -- yarn dev
+# dapr run --app-id interviewstopwatch --app-port 5173 --dapr-http-port 3500 -- yarn dev
+
+yarn dev
 
 wait
 
